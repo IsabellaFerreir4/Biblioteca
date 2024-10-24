c6b25f90105455ba32d4367f2fa40a0c2220036b
IsabellaFerreir4 <if1823107@gmail.com>
2024-10-24 16:57:27 -0300

    Migrações

---
+++ Migrations/20241024194124_Migracao.cs
@@ -0,0 +1,156 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Exemplo6.Migrations
+{
+    /// <inheritdoc />
+    public partial class Migracao : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Autor",
+                columns: table => new
+                {
+                    AutorID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    DataNascimento = table.Column<DateOnly>(type: "date", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Autor", x => x.AutorID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Genero",
+                columns: table => new
+                {
+                    GeneroID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Nome = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Genero", x => x.GeneroID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Endereco",
+                columns: table => new
+                {
+                    EnderecoID = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    Logradouro = table.Column<string>(type: "character varying(80)", maxLength: 80, nullable: false),
+                    Cidade = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false),
+                    UF = table.Column<string>(type: "character varying(2)", maxLength: 2, nullable: false),
+                    CEP = table.Column<string>(type: "character varying(8)", maxLength: 8, nullable: false),
+                    AutorID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Endereco", x => x.EnderecoID);
+                    table.ForeignKey(
+                        name: "FK_Endereco_Autor_AutorID",
+                        column: x => x.AutorID,
+                        principalTable: "Autor",
+                        principalColumn: "AutorID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Livro",
+                columns: table => new
+                {
+                    ISBN = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    Titulo = table.Column<string>(type: "character varying(120)", maxLength: 120, nullable: false),
+                    Paginas = table.Column<int>(type: "integer", nullable: true),
+                    AutorID = table.Column<int>(type: "integer", nullable: false),
+                    Estoque = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Livro", x => x.ISBN);
+                    table.ForeignKey(
+                        name: "FK_Livro_Autor_AutorID",
+                        column: x => x.AutorID,
+                        principalTable: "Autor",
+                        principalColumn: "AutorID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "LivroGenero",
+                columns: table => new
+                {
+                    ISBN = table.Column<string>(type: "character varying(13)", maxLength: 13, nullable: false),
+                    GeneroID = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_LivroGenero", x => new { x.ISBN, x.GeneroID });
+                    table.ForeignKey(
+                        name: "FK_LivroGenero_Genero_GeneroID",
+                        column: x => x.GeneroID,
+                        principalTable: "Genero",
+                        principalColumn: "GeneroID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_LivroGenero_Livro_ISBN",
+                        column: x => x.ISBN,
+                        principalTable: "Livro",
+                        principalColumn: "ISBN",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Autor_Nome",
+                table: "Autor",
+                column: "Nome");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Endereco_AutorID",
+                table: "Endereco",
+                column: "AutorID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livro_AutorID",
+                table: "Livro",
+                column: "AutorID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Livro_Titulo",
+                table: "Livro",
+                column: "Titulo");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_LivroGenero_GeneroID",
+                table: "LivroGenero",
+                column: "GeneroID");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Endereco");
+
+            migrationBuilder.DropTable(
+                name: "LivroGenero");
+
+            migrationBuilder.DropTable(
+                name: "Genero");
+
+            migrationBuilder.DropTable(
+                name: "Livro");
+
+            migrationBuilder.DropTable(
+                name: "Autor");
+        }
+    }
+}
